791054409335010b23585f93bdbae83ab9a3d957
Evan Kim <dkim2435@gmail.com>
2019-08-13 15:47:35 -0400

    added order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchaseable: false
+  }
+
+  updatePurchaseState(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey]
+      })
+      .reduce((sum, el) => {
+        return sum + el
+      }, 0)
+    this.setState({ purchaseable: sum > 0 })
   }
 
   addIngredientHandler = (type) => {
@@ -32,6 +44,8 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice
     const newPrice = oldPrice + priceAddition
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePurchaseState(updatedIngredients)
+
   }
 
   removeInfredientHandler = (type) => {
@@ -48,7 +62,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice
     const newPrice = oldPrice - priceDeduction
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
-
+    this.updatePurchaseState(updatedIngredients)
   }
 
   render() {
@@ -65,6 +79,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeInfredientHandler}
           disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
           price={this.state.totalPrice} />
       </Aux>
     )
