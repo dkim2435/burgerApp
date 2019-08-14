c6fba6e83993aae478cba26254ac451399e02b5c
Evan Kim <dkim2435@gmail.com>
2019-08-13 22:11:51 -0400

    added side drawer toggle (for mobile devices)

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,9 +3,9 @@ import classes from './Logo.module.css'
 import burgerLogo from '../../assets/images/burger-logo.png'
 
 const logo = (props) => (
-  <div className={classes.Logo}>
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={burgerLogo} alt="MyBurger" />
-  </div>
+  </div >
 )
 
 export default logo
