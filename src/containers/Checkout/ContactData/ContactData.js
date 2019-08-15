7ce9763c830f71bd3f9fc922976bb802710cf5b4
Evan Kim <dkim2435@gmail.com>
2019-08-15 14:18:13 -0400

    handles form submission and is added to firebase database

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,14 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault()
     this.setState({ loading: true })
+    const formData = {}
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -103,18 +108,18 @@ class ContactData extends Component {
       })
     }
 
-    let form = (<form>
-
-      {formElementsArray.map(formElement => (
-        <Input
-          key={formElement.id}
-          elementType={formElement.config.elementType}
-          elementConfig={formElement.config.elementConfig}
-          value={formElement.config.value}
-          changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-      ))}
-      <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-    </form>)
+    let form = (
+      <form onSubmit={this.orderHandler}>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+        ))}
+        <Button btnType="Success" >ORDER</Button>
+      </form>)
 
     if (this.state.loading) {
       form = <Spinner />
