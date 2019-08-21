0a70b148edaa1a6b0c4c0823629e8aa6e02886b1
Evan Kim <dkim2435@gmail.com>
2019-08-21 00:19:03 -0400

    handling purchases and updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import ContactData from './ContactData/ContactData'
 import { connect } from 'react-redux'
+
 
 
 class Checkout extends Component {
@@ -16,24 +17,33 @@ class Checkout extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedhandler} />
-        <Route path={this.props.match.path + '/contact-data'}
-          component={ContactData} />
-      </div>
-    )
+    let summary = <Redirect to="/" />
+    if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null
+      summary = (
+        <div>
+          {purchasedRedirect}
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedhandler} />
+          <Route path={this.props.match.path + '/contact-data'}
+            component={ContactData} />
+        </div>
+      )
+    }
+    return summary
   }
 }
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients
+    ings: state.burgerBuilder.ingredients,
     // ingredient is what I named in reducer, cannot be anyname, but ings is something that i can just come up with
+    purchased: state.order.purchased
   }
 }
+
+
 
 export default connect(mapStateToProps)(Checkout)
