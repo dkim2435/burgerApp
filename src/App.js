472ed9cc906488914ba39bc7b99f5b1c67866275
Evan Kim <dkim2435@gmail.com>
2019-08-21 18:59:25 -0400

    fixed connect and routing errors

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import { connect } from 'react-redux'
+import * as actions from './store/actions/index'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup()
+  }
 
   render() {
     return (
@@ -26,4 +31,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
