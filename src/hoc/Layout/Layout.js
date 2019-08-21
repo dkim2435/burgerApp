1b94eb343580761f6334b2464ac1904ce38a3a66
Evan Kim <dkim2435@gmail.com>
2019-08-21 15:47:56 -0400

    updating the navigation item depending on Auth state (logged in || logged out)

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,6 +3,7 @@ import classes from './Layout.module.css'
 import Aux from '../Aux/Aux'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+import { connect } from 'react-redux'
 
 class Layout extends Component {
 
@@ -23,8 +24,11 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler} />
         <main className={classes.Content}>
@@ -35,5 +39,11 @@ class Layout extends Component {
   }
 }
 
-export default Layout
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
 
